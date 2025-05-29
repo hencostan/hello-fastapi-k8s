9cdc0253fcc2c407cfdec45537cf2dcf56c001f3
Henrique Costa <hencostnascimento@gmail.com>
2025-05-29 09:39:46 -0300

    feat: Atualização do main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,14 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World!"}
 
+@app.get("/square/{x}")
+def square(x: int):
+    return {"result": x + x}
+
+@app.get("/double/{x}")
+def double(x: int):
+    return {"result": x * 2}
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
